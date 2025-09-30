a7dce2f752eb3246f8e612685b7a31a10c9fb989
bruncastro <118383592+bruncastro@users.noreply.github.com>
2025-09-30 13:28:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/experiencia/", // garante que todos os assets sejam servidos da subpasta /experiencia
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
